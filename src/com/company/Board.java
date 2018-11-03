f160d2fb7b17abf437164932379d1cf10179ab90
Sabelle Huang <sabelle2@illinois.edu>
2018-11-03 15:32:50 -0500

    graphics

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -7,26 +7,15 @@ public class Board extends JPanel {
     public static final Color PURPLE = new Color(241, 216, 246);
     public static final Color PINK = new Color(254, 224, 234);
     public static final Color WHITE = new Color(255, 255, 255);
-    public static final Color[] COLORS = {BLUE, PURPLE, PINK, WHITE};
+    public static final Color[] COLORS = {BLUE, BLUE, PURPLE, PINK, WHITE};
 
     public Board() {
         repaint();
     }
     public void paint(Graphics g) {
-        int color = (int)(Math.random() * 3 + 1);
-        g.setColor(COLORS[color]);
-        g.fillRect(100,100,50,50);
-        color = (int)(Math.random() * 3 + 1);
-        g.setColor(COLORS[color]);
-        g.fillRect(150,100,50,50);
-        color = (int)(Math.random() * 3 + 1);
-        g.setColor(COLORS[color]);
-        g.fillRect(200,100,50,50);
-
-
         for (int x = 100; x < 600; x+=50) {
-            for (int y = 100; y < 600; y+=50) {
-                color = (int)(Math.random() * 3 + 1);
+            for (int y = 50; y < 600; y+=50) {
+                int color = (int)(Math.random() * 4 + 1);
                 g.setColor(COLORS[color]);
                 g.fillRect(x, y, 50, 50);
             }
