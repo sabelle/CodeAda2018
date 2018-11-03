bea703b158496830d877824f709ab3b958609671
Sarah Zhou <szhou123@gmail.com>
2018-11-03 16:29:13 -0500

    made some changes

--- src/com/company/DiscoMain.java
+++ src/com/company/DiscoMain.java
@@ -4,77 +4,38 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class DiscoMain extends JPanel {     // main class for the game
-    // Define constants for the game
+public class DiscoMain extends JPanel {
     private static final int CANVAS_WIDTH = 700;
     private static final int CANVAS_HEIGHT = 700;
 
     public DiscoMain() {
-        gameInit();
-
         GameCanvas canvas = new GameCanvas();
-        ControlPanel panel = new ControlPanel(canvas);
         canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
         this.add(canvas, BorderLayout.SOUTH);
-        this.add(panel, BorderLayout.NORTH);
         this.setVisible(true);
     }
 
-    // Initialize all the game objects, run only once.
-    public void gameInit() {
-    }
-
-    // Start and re-start the game.
-    public void gameStart() {
-
-    }
-
-    // Shutdown the game, clean up code that runs only once.
-    public void gameShutdown() {
-
-    }
-
-    // One step of the game.
-    public void gameUpdate() {
-
-    }
-
-    // Refresh the display after each step.
-    // Use (Graphics g) as argument if you are not using Java 2D.
     public void gameDraw(Graphics2D g2d) {
         Board newBoard = new Board();
         newBoard.paint(g2d);
     }
 
-    // Process a key-pressed event.
-    public void gameKeyPressed(KeyEvent e) {
-    }
-
-    // Other methods
-    // ......
-
-    // Custom drawing panel, written as an inner class.
     class GameCanvas extends JPanel implements KeyListener {
-        // Constructor
         public GameCanvas() {
             setFocusable(true);  // so that this can receive key-events
             requestFocus();
             addKeyListener(this);
         }
 
-        // Override paintComponent to do custom drawing.
-        // Called back by repaint().
         @Override
         public void paintComponent(Graphics g) {
             Graphics2D g2d = (Graphics2D)g;  // if using Java 2D
             super.paintComponent(g2d);       // paint background
             setBackground(Color.GRAY);      // may use an image for background
 
-            // Draw the game objects
             gameDraw(g2d);
         }
 
-        // KeyEvent handlers
         @Override
         public void keyPressed(KeyEvent e) {
         }
@@ -86,7 +47,7 @@ public class DiscoMain extends JPanel {     // main class for the game
                 revalidate();
                 repaint();
             }
-        }  // not used
+        }
 
         @Override
         public void keyTyped(KeyEvent e) { }     // not used
@@ -99,18 +60,14 @@ public class DiscoMain extends JPanel {     // main class for the game
             @Override
             public void run() {
                 Board newBoard = new Board();
-                JFrame frame = new JFrame("Flood It");
+                JFrame frame = new JFrame("Disco in a Box!!");
                 frame.add(newBoard);
                 frame.setVisible(true);
-
-                // Set the content-pane of the JFrame to an instance of main JPanel
-                frame.setContentPane(new GameMain());  // main JPanel as content pane
-                //frame.setJMenuBar(menuBar);          // menu-bar (if defined)
+                frame.setContentPane(new DiscoMain());
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();
                 frame.setLocationRelativeTo(null); // center the application window
                 frame.setVisible(true);            // show it
-
             }
         });
     }
